4c1780c1bcb35cf09db484227747b54d633c6d28
lunexnocty <lunex_nocty@qq.com>
2023-11-21 04:10:38 +0800

    :construction: add an example of calculator, add chainl1 and chainr1

--- src/typeclass.py
+++ src/typeclass.py
@@ -1,9 +1,8 @@
-from ast import TypeVar
 from typing import Callable, Type, Protocol, Self, cast
 from typing import runtime_checkable
 from abc import abstractmethod, abstractclassmethod
 
-from utils import cons, identity
+from src.utils import cons, identity
 
 type Lazy[T] = Callable[[], T]
 
@@ -13,7 +12,7 @@ class Functor[T](Protocol):
     @abstractmethod
     def map[T1](
         self: Self,
-        fn: Callable[[T], T1]
+        _fn: Callable[[T], T1]
     ) -> 'Functor[T1]':
         raise NotImplementedError()
     
@@ -28,26 +27,26 @@ class Applicative[T](Functor[T], Protocol):
     @abstractclassmethod
     def of(
         cls: Type[Self],
-        value: T
+        _value: T
     ) -> Self:
         raise NotImplementedError()
     
     @abstractmethod
     def apply[T1](
         self: 'Applicative[Callable[[T], T1]]',
-        m1: Lazy['Applicative[T]'],
+        _m: Lazy['Applicative[T]'],
     ) -> 'Applicative[T1]':
         raise NotImplementedError()
 
     def liftA2[T1, T2](
         self: Self,
-        fn: Callable[[T], Callable[[T1], T2]],
-        m1: Lazy['Applicative[T1]']
+        _fn: Callable[[T], Callable[[T1], T2]],
+        _m: Lazy['Applicative[T1]']
     ) -> 'Applicative[T2]':
-        return cast(Applicative, self.map(fn)).apply(m1)
+        return cast(Applicative, self.map(_fn)).apply(_m)
     
-    def count(self, n: int) -> 'Applicative[list[T]]':
-        return self.of([]) if n == 0 else self.liftA2(cons, lambda:self.count(n-1))
+    def count(self, _n: int) -> 'Applicative[list[T]]':
+        return self.of([]) if _n == 0 else self.liftA2(cons, lambda:self.count(_n - 1))
 
 @runtime_checkable
 class Alternative[T](Applicative[T], Protocol):
@@ -55,7 +54,7 @@ class Alternative[T](Applicative[T], Protocol):
     @abstractclassmethod
     def of(
         cls: Type[Self],
-        value: T
+        _value: T
     ) -> Self:
         raise NotImplementedError()
 
@@ -68,16 +67,16 @@ class Alternative[T](Applicative[T], Protocol):
     @abstractmethod
     def alter_[T1](
         self: Self,
-        m2: 'Alternative[T1]'
+        _m: 'Alternative[T1]'
     ) -> 'Alternative[T | T1]':
         raise NotImplementedError()
     
     @staticmethod
     def alter[T1, T2](
-        m1: 'Alternative[T1]',
-        m2: 'Alternative[T2]'
+        _m1: 'Alternative[T1]',
+        _m2: 'Alternative[T2]'
     ) -> 'Alternative[T1 | T2]':
-        return m1.alter_(m2)
+        return _m1.alter_(_m2)
 
     def some(self: Self) -> 'Alternative[list[T]]':
         return cast(Alternative, self.liftA2(cons, self.many))
@@ -91,26 +90,26 @@ class Monad[T](Applicative[T], Protocol):
     @abstractclassmethod
     def of(
         cls: Type[Self],
-        value: T
+        _value: T
     ) -> Self:
         raise NotImplementedError()
 
     @abstractmethod
     def bind[T1](
         self: Self,
-        fn: Callable[[T], 'Monad[T1]']
+        _fn: Callable[[T], 'Monad[T1]']
     ) -> 'Monad[T1]':
         raise NotImplementedError()
     
-    def pair_[T1](self, m1: 'Monad[T1]'):
-        return self.bind(lambda v1: m1.bind(lambda v2: self.of((v1, v2))))
+    def pair_[T1](self, _m: 'Monad[T1]'):
+        return self.bind(lambda v1: _m.bind(lambda v2: self.of((v1, v2))))
     
     @staticmethod
     def pair[T1, T2](
-        m1: 'Monad[T1]',
-        m2: 'Monad[T2]'
+        _m1: 'Monad[T1]',
+        _m2: 'Monad[T2]'
     ):
-        return m1.pair_(m2)
+        return _m1.pair_(_m2)
     
     def join(self: 'Monad[Monad[T]]') -> 'Monad[T]':
         return self.bind(identity)
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-
-__src__ = 'src'
-
-src = os.path.join(os.getcwd(), __src__)
-sys.path.append((src))
