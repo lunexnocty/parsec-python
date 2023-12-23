6ba326b4e8675b68fcec1d67607ef02d190e5343
lunexnocty <lunex_nocty@qq.com>
2023-12-23 18:09:07 +0800

    :construction: add pos status

--- src/parsec.py
+++ src/parsec.py
@@ -1,35 +1,42 @@
-
-from email import message
-from typing import Any, Type, NamedTuple, Callable, Iterable
+from typing import Any, Type, NamedTuple, Callable, Iterable, Sequence, Union, Self, NewType
 from typing import cast, overload
+from abc import ABC, abstractmethod
+import os
 from functools import reduce
 
 from src.utils import const, fst, snd
 
-class Pos(NamedTuple):
-    line: int = 1
-    column: int = 1
+class State[I](ABC):
+    @abstractmethod
+    def update(self, val: I) -> Self:
+        raise NotImplementedError()
 
-class State:
-    def __init__(self, _input: str):
-        self.data = _input
-        self.pos = Pos()
+class Input[I]:
+    def __init__(self, _in: Sequence[I], _st: State[I], _cursor = 0):
+        self.data = _in
+        self.cursor = _cursor
+        self.state = _st
+    
+    def get(self) -> tuple[I | None, 'Input[I]']:
+        if self.cursor >= len(self.data): return None, self
+        val = self.data[self.cursor]
+        return val, Input(self.data, self.state.update(val),  self.cursor + 1)
 
-class Okay[T](NamedTuple):
+class Okay[I, T](NamedTuple):
     value: T
-    rest: str
+    rest: Input[I]
 
 class Fail(NamedTuple):
     message: list[str]
 
-type Result[T] = Okay[T] | Fail
+type Result[I, T] = Okay[I, T] | Fail
 
-class Parser[T]:
-    def __init__(self, _fn: Callable[[str], Result[T]]) -> None:
+class Parser[I, T]:
+    def __init__(self, _fn: Callable[[Input[I]], Result[I, T]]) -> None:
         self.parse = _fn
     
-    def __call__(self, _input: str):
-        return self.parse(_input)
+    def __call__(self, _in: Input[I]):
+        return self.parse(_in)
 
     @classmethod
     def okay(cls, _val: T):
@@ -40,48 +47,48 @@ class Parser[T]:
         return cls(const(Fail(_err)))
 
     def bind[T1](
-        self: 'Parser[T]',
-        _fn: Callable[[T], 'Parser[T1]']
-    ):
+        self: 'Parser[I, T]',
+        _fn: Callable[[T], 'Parser[I, T1]']
+    ) -> 'Parser[I, T1]':
         @Parser
-        def parse(_input: str) -> Result[T1]:
-            match self(_input):
+        def parse(_in: Input[I]) -> Result[I, T1]:
+            match self(_in):
                 case Okay(value=val, rest=rest): return _fn(val)(rest)
                 case Fail(message=err): return Fail(err)
         return parse
         
-    def map[T1](self: 'Parser[T]', _fn: Callable[[T], T1]):
+    def map[T1](self: 'Parser[I, T]', _fn: Callable[[T], T1]) -> 'Parser[I, T1]':
         return self.bind(lambda x: Parser.okay(_fn(x)))
     
     def error(self, _fn: Callable[[list[str]], list[str]]):
         @Parser
-        def parse(_input: str) -> Result[T]:
-            match self(_input):
+        def parse(_in: Input[I]) -> Result[I, T]:
+            match self(_in):
                 case Okay(value=val, rest=rest): return Okay(val, rest)
                 case Fail(message=err): return Fail(_fn(err))
         return parse
     
     def apply[T1](
-        self: 'Parser[Callable[[T], T1]]',
-        _p: 'Parser[T]',
-    ):
+        self: 'Parser[I, Callable[[T], T1]]',
+        _p: 'Parser[I, T]',
+    ) -> 'Parser[I, T1]':
         return self.bind(_p.map)
 
     def alter_[T1](
         self,
-        _p: 'Parser[T1]'
-    ):
+        _p: 'Parser[I, T1]'
+    ) -> 'Parser[I, T | T1]':
         @Parser
-        def parse(_input: str) -> Result[T | T1]:
-            match self(_input):
+        def parse(_in: Input[I]) -> Result[I, T | T1]:
+            match self(_in):
                 case Okay(value=val, rest=rest): return Okay(val, rest)
                 case Fail(message=e1):
-                    match _p(_input):
+                    match _p(_in):
                         case Okay(value=val, rest=rest): return Okay(val, rest)
                         case Fail(message=e2): return Fail(e1 + e2)
         return parse
 
-    def repeat(self, _n: int) -> 'Parser[list[T]]':
+    def repeat(self, _n: int) -> 'Parser[I, list[T]]':
         if _n == 0: return Parser.okay([])
         return self.bind(lambda x: self.repeat(_n - 1).bind(lambda xs: Parser.okay([x, *xs])))
     
@@ -93,36 +100,36 @@ class Parser[T]:
 
     def many(self):
         @Parser
-        def parse(_input: str) -> Result[list[T]]:
+        def parse(_in: Input[I]) -> Result[I, list[T]]:
             results: list[T] = []
-            src = _input
+            cur_in = _in
             while True:
-                match self(src):
+                match self(cur_in):
                     case Okay(value=val, rest=rest):
                         results.append(val)
-                        src = rest
-                    case Fail(): return Okay(results, src)
+                        cur_in = rest
+                    case Fail(): return Okay(results, cur_in)
         return parse            
 
     @overload
-    def str(self: 'Parser[list[str]]') -> 'Parser[str]': ...
+    def str(self: 'Parser[I, list[str]]') -> 'Parser[I, str]': ...
 
     @overload
-    def str(self: 'Parser[tuple[str, ...]]') -> 'Parser[str]': ...
+    def str(self: 'Parser[I, tuple[str, ...]]') -> 'Parser[I, str]': ...
 
-    def str(self: 'Parser') -> 'Parser[str]':
+    def str(self: 'Parser') -> 'Parser[I, str]':
         def _fn1(val: Iterable[str]) -> str:
             return ''.join(val)
         return self.map(_fn1)
     
-    def at[*Ts](self: 'Parser[tuple[*Ts]]', _index: int):
+    def at[*Ts](self: 'Parser[I, tuple[*Ts]]', _index: int) -> 'Parser[I, Union[*Ts]]':
         def _fn1(val: tuple[*Ts]):
             assert 0 <= _index < len(val), f'{_index=}, {val=}'
             return val[_index]
         return self.map(_fn1)
     
-    def as_type[T1](self, tp: Type[T1]):
-        return cast(Parser[tp], self)
+    def as_type[T1](self, tp: Type[T1]) -> 'Parser[I, T1]':
+        return cast(Parser[I, tp], self)
     
     def maybe(self):
         return self.alter_(Parser.okay(None))
@@ -136,203 +143,203 @@ class Parser[T]:
     def neq(self, _val: T):
         return self.where(lambda v: v != _val, f'not expected `{_val}`')
    
-    def prefix[T1](self, _p: 'Parser[T1]'):
+    def prefix(self, _p: 'Parser[I, Any]'):
         return pair(_p, self).map(snd)
     
-    def suffix[T1](self, _p: 'Parser[T1]'):
+    def suffix(self, _p: 'Parser[I, Any]'):
         return pair(self, _p).map(fst)
 
-    def between[T1, T2](self, _left: 'Parser[T1]', _right: 'Parser[T2]'):
+    def between(self, _left: 'Parser[I, Any]', _right: 'Parser[I, Any]'):
         return self.prefix(_left).suffix(_right)
 
-    def trim[T1](self, _p: 'Parser[T1]'):
-        return self.between(_p.many(), _p.many())
-    
-    def ltrim[T1](self, _p: 'Parser[T1]'):
+    def ltrim(self, _p: 'Parser[I, Any]'):
         return self.prefix(_p.many())
-    
-    def rtrim[T1](self, _p: 'Parser[T1]'):
+
+    def rtrim(self, _p: 'Parser[I, Any]'):
         return self.suffix(_p.many())
-    
-    def default[T1](self, _val: T1):
+
+    def trim(self, _p: 'Parser[I, Any]'):
+        return self.ltrim(_p).rtrim(_p)
+
+    def default[T1](self, _val: T1) -> 'Parser[I, T | T1]':
         return self.alter_(Parser.okay(_val))
      
-    def sep_by[T1](self, _sep: 'Parser[T1]'):
+    def sep_by(self, _sep: 'Parser[I, Any]'):
         remains = self.prefix(_sep).many()
         return self.bind(lambda x: remains.bind(lambda xs: Parser.okay([x, *xs])))
 
-    def chainl1(self, _op: 'Parser[Callable[[T], Callable[[T], T]]]'):
-        def rest(x: T) -> Parser[T]:
+    def chainl1(self, _op: 'Parser[I, Callable[[T], Callable[[T], T]]]'):
+        def rest(x: T) -> Parser[I, T]:
             return _op.bind(lambda op: self.bind(lambda y: rest(op(x)(y)))).alter_(Parser.okay(x))
         return self.bind(lambda x: rest(x))
     
-    def chainr1(self, _op: 'Parser[Callable[[T], Callable[[T], T]]]'):
+    def chainr1(self, _op: 'Parser[I, Callable[[T], Callable[[T], T]]]'):
         scan = self.bind(lambda x: rest(x))
-        def rest(x: T) -> Parser[T]:
+        def rest(x: T) -> Parser[I, T]:
             return _op.bind(lambda op: scan.bind(lambda y: rest(op(x)(y))))
         return scan
 
 
-def alter[T1, T2](_p1: Parser[T1], _p2: Parser[T2]):
+def alter[I, T1, T2](_p1: Parser[I, T1], _p2: Parser[I, T2]) -> Parser[I, T1 | T2]:
     return _p1.alter_(_p2)
 
-def pair[T1, T2](_p1: Parser[T1], _p2: Parser[T2]):
+def pair[I, T1, T2](_p1: Parser[I, T1], _p2: Parser[I, T2]) -> Parser[I, tuple[T1, T2]]:
     return _p1.bind(lambda v1: _p2.bind(lambda v2: Parser.okay((v1, v2))))
 
-def sel[T](*_plist: Parser[T]):
+def sel[I, T](*_plist: Parser[I, T]) -> Parser[I, T]:
     return reduce(alter, _plist)
 
-def seq[T](*_plist: Parser[T]):
+def seq[I, T](*_plist: Parser[I, T]) -> Parser[I, list[T]]:
     @Parser
-    def parse(_input: str) -> Result[list[T]]:
+    def parse(_in: Input[I]) -> Result[I, list[T]]:
         results: list[T] = []
-        src = _input
+        cur_in = _in
         for p in _plist:
-            match p(src):
+            match p(cur_in):
                 case Okay(value=val, rest=rest):
                     results.append(val)
-                    src = rest
+                    cur_in = rest
                 case Fail(message=err):
                     return Fail(err)
-        return Okay(results, src)
+        return Okay(results, cur_in)
     return parse
 
 @overload
-def hsel[T1, T2](
-    _p1: Parser[T1], _p2: Parser[T2]
-) -> Parser[T1 | T2]: ...
+def hsel[I, T1, T2](
+    _p1: Parser[I, T1], _p2: Parser[I, T2]
+) -> Parser[I, T1 | T2]: ...
     
 @overload
-def hsel[T1, T2, T3](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3]
-) -> Parser[T1 | T2 | T3]: ...
+def hsel[I, T1, T2, T3](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3]
+) -> Parser[I, T1 | T2 | T3]: ...
     
 @overload
-def hsel[T1, T2, T3, T4](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4]
-) -> Parser[T1 | T2 | T3 | T4]: ...
+def hsel[I, T1, T2, T3, T4](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4]
+) -> Parser[I, T1 | T2 | T3 | T4]: ...
     
 @overload
-def hsel[T1, T2, T3, T4, T5](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5]
-) -> Parser[T1 | T2 | T3 | T4 | T5]: ...
+def hsel[I, T1, T2, T3, T4, T5](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5]
+) -> Parser[I, T1 | T2 | T3 | T4 | T5]: ...
     
 @overload
-def hsel[T1, T2, T3, T4, T5, T6](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6]
-) -> Parser[T1 | T2 | T3 | T4 | T5 | T6]: ...
+def hsel[I, T1, T2, T3, T4, T5, T6](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6]
+) -> Parser[I, T1 | T2 | T3 | T4 | T5 | T6]: ...
     
 @overload
-def hsel[T1, T2, T3, T4, T5, T6, T7](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7]
-) -> Parser[T1 | T2 | T3 | T4 | T5 | T6 | T7]: ...
+def hsel[I, T1, T2, T3, T4, T5, T6, T7](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7]
+) -> Parser[I, T1 | T2 | T3 | T4 | T5 | T6 | T7]: ...
     
 @overload
-def hsel[T1, T2, T3, T4, T5, T6, T7, T8](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7], _p8: Parser[T8]
-) -> Parser[T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8]: ...
+def hsel[I, T1, T2, T3, T4, T5, T6, T7, T8](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7], _p8: Parser[I, T8]
+) -> Parser[I, T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8]: ...
     
 @overload
-def hsel[T1, T2, T3, T4, T5, T6, T7, T8, T9](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7], _p8: Parser[T8],
-    _p9: Parser[T9]
-) -> Parser[T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9]: ...
+def hsel[I, T1, T2, T3, T4, T5, T6, T7, T8, T9](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7], _p8: Parser[I, T8],
+    _p9: Parser[I, T9]
+) -> Parser[I, T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9]: ...
 
-def hsel[T1, T2, T3, T4, T5, T6, T7, T8, T9](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3] | None = None,
-    _p4: Parser[T4] | None = None,
-    _p5: Parser[T5] | None = None,
-    _p6: Parser[T6] | None = None,
-    _p7: Parser[T7] | None = None,
-    _p8: Parser[T8] | None = None,
-    _p9: Parser[T9] | None = None,
-    *_ps: Parser[Any]
-):
-    plist: list[Parser[Any]] = list(filter(None, (_p1, _p2, _p3, _p4, _p5, _p6, _p7, _p8, _p9, *_ps)))
+def hsel[I](
+    _p1: Parser[I, Any], _p2: Parser[I, Any],
+    _p3: Parser[I, Any] | None = None,
+    _p4: Parser[I, Any] | None = None,
+    _p5: Parser[I, Any] | None = None,
+    _p6: Parser[I, Any] | None = None,
+    _p7: Parser[I, Any] | None = None,
+    _p8: Parser[I, Any] | None = None,
+    _p9: Parser[I, Any] | None = None,
+    *_ps: Parser[I, Any]
+) -> Parser[I, Any]:
+    plist: list[Parser[I, Any]] = list(filter(None, (_p1, _p2, _p3, _p4, _p5, _p6, _p7, _p8, _p9, *_ps)))
     return sel(*plist)
 
 @overload
-def hseq[T1, T2](
-    _p1: Parser[T1], _p2: Parser[T2]
-) -> Parser[tuple[T1, T2]]: ...
+def hseq[I, T1, T2](
+    _p1: Parser[I, T1], _p2: Parser[I, T2]
+) -> Parser[I, tuple[T1, T2]]: ...
     
 @overload
-def hseq[T1, T2, T3](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3]
-) -> Parser[tuple[T1, T2, T3]]: ...
+def hseq[I, T1, T2, T3](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3]
+) -> Parser[I, tuple[T1, T2, T3]]: ...
     
 @overload
-def hseq[T1, T2, T3, T4](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4]
-) -> Parser[tuple[T1, T2, T3, T4]]: ...
+def hseq[I, T1, T2, T3, T4](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4]
+) -> Parser[I, tuple[T1, T2, T3, T4]]: ...
     
 @overload
-def hseq[T1, T2, T3, T4, T5](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5]
-) -> Parser[tuple[T1, T2, T3, T4, T5]]: ...
+def hseq[I, T1, T2, T3, T4, T5](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5]
+) -> Parser[I, tuple[T1, T2, T3, T4, T5]]: ...
     
 @overload
-def hseq[T1, T2, T3, T4, T5, T6](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6]
-) -> Parser[tuple[T1, T2, T3, T4, T5, T6]]: ...
+def hseq[I, T1, T2, T3, T4, T5, T6](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6]
+) -> Parser[I, tuple[T1, T2, T3, T4, T5, T6]]: ...
 
 @overload
-def hseq[T1, T2, T3, T4, T5, T6, T7](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7]
-) -> Parser[tuple[T1, T2, T3, T4, T5, T6, T7]]: ...
+def hseq[I, T1, T2, T3, T4, T5, T6, T7](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7]
+) -> Parser[I, tuple[T1, T2, T3, T4, T5, T6, T7]]: ...
     
 @overload
-def hseq[T1, T2, T3, T4, T5, T6, T7, T8](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7], _p8: Parser[T8]
-) -> Parser[tuple[T1, T2, T3, T4, T5, T6, T7, T8]]: ...
+def hseq[I, T1, T2, T3, T4, T5, T6, T7, T8](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7], _p8: Parser[I, T8]
+) -> Parser[I, tuple[T1, T2, T3, T4, T5, T6, T7, T8]]: ...
     
 @overload
-def hseq[T1, T2, T3, T4, T5, T6, T7, T8, T9](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3], _p4: Parser[T4],
-    _p5: Parser[T5], _p6: Parser[T6],
-    _p7: Parser[T7], _p8: Parser[T8],
-    _p9: Parser[T9]
-) -> Parser[tuple[T1, T2, T3, T4, T5, T6, T7, T8, T9]]: ...
+def hseq[I, T1, T2, T3, T4, T5, T6, T7, T8, T9](
+    _p1: Parser[I, T1], _p2: Parser[I, T2],
+    _p3: Parser[I, T3], _p4: Parser[I, T4],
+    _p5: Parser[I, T5], _p6: Parser[I, T6],
+    _p7: Parser[I, T7], _p8: Parser[I, T8],
+    _p9: Parser[I, T9]
+) -> Parser[I, tuple[T1, T2, T3, T4, T5, T6, T7, T8, T9]]: ...
 
-def hseq[T1, T2, T3, T4, T5, T6, T7, T8, T9](
-    _p1: Parser[T1], _p2: Parser[T2],
-    _p3: Parser[T3] | None = None,
-    _p4: Parser[T4] | None = None,
-    _p5: Parser[T5] | None = None,
-    _p6: Parser[T6] | None = None,
-    _p7: Parser[T7] | None = None,
-    _p8: Parser[T8] | None = None,
-    _p9: Parser[T9] | None = None,
-    *_ps: Parser[Any]
+def hseq[I](
+    _p1: Parser[I, Any], _p2: Parser[I, Any],
+    _p3: Parser[I, Any] | None = None,
+    _p4: Parser[I, Any] | None = None,
+    _p5: Parser[I, Any] | None = None,
+    _p6: Parser[I, Any] | None = None,
+    _p7: Parser[I, Any] | None = None,
+    _p8: Parser[I, Any] | None = None,
+    _p9: Parser[I, Any] | None = None,
+    *_ps: Parser[I, Any]
 ):
-    plist: list[Parser[Any]] = list(filter(None, (_p1, _p2, _p3, _p4, _p5, _p6, _p7, _p8, _p9, *_ps)))
+    plist: list[Parser[I, Any]] = list(filter(None, (_p1, _p2, _p3, _p4, _p5, _p6, _p7, _p8, _p9, *_ps)))
     return seq(*plist).map(tuple)
