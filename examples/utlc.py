643a88e0cc269605dc40f2c17008fac4a87850be
lunexnocty <lunex_nocty@qq.com>
2025-05-09 14:11:24 +0800

    :beers: 增加λ解析器实现样例

--- examples/utlc.py
+++ examples/utlc.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from core import Parser
+from text import lexeme
+
 """
 AST of Lambda
 >>> Expr = Var
@@ -35,3 +38,9 @@ class App(Expr):
 >>> abs  := '\\' <var>+ '->' <expr>
 >>> app  := <expr> <expr>
 """
+
+expr = Parser[str, Expr]()
+var = lexeme.lex_identifier.map(Var)
+abs = (var.prefix(lexeme.lex_char('\\')) & expr.prefix(lexeme.lex_literal('->'))).map(lambda t: Abs(t[0], t[1]))
+app = (expr & expr).map(lambda t: App(t[0], t[1]))
+expr.define(var | abs | app | expr.between(lexeme.lex_l_round, lexeme.lex_r_round))
